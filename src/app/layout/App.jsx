e425a1bb909020bf469359491266a4a97604f785
Farkass47 <sandor.farkas@t-online.hu>
2020-03-20 19:09:39 +0100

    End of section 6

--- src/app/layout/App.jsx
+++ src/app/layout/App.jsx
@@ -1,16 +1,36 @@
 import React, { Component, Fragment } from "react";
-import EventDashboard from "../../features/event/EventDashboard/EventDashboard";
+import EventDashboard from "../../features/events/EventDashboard/EventDashboard";
+import EventDetailedPage from "../../features/events/EventDetailed/EventdetailedPage";
+import PeopleDashboard from "../../features/user/Peopledashboard/PeopleDashboard";
+import UserDetailedPage from "../../features/user/UserDetailed/UserDetailedPage";
+import SettingDashboard from "../../features/user/Settings/SettingsDashboard";
+import HomePage from "../../features/home/HomePage";
+import EventForm from "../../features/events/EventForm/EventForm";
 import Navbar from "../../features/nav/Navbar/Navbar";
 import { Container } from "semantic-ui-react";
+import { Route } from "react-router-dom";
 
 class App extends Component {
   render() {
     return (
       <Fragment>
-        <Navbar />
-        <Container className="main">
-          <EventDashboard />
-        </Container>
+        <Route exact path="/" component={HomePage} />
+        <Route
+          path="/(.+)"
+          render={() => (
+            <Fragment>
+              <Navbar />
+              <Container className="main">
+                <Route path="/events" component={EventDashboard} />
+                <Route path="/events/:id" component={EventDetailedPage} />
+                <Route path="/people" component={PeopleDashboard} />
+                <Route path="/profile/:id" component={UserDetailedPage} />
+                <Route path="/settings" component={SettingDashboard} />
+                <Route path="/createEvent/:id" component={EventForm} />
+              </Container>
+            </Fragment>
+          )}
+        />
       </Fragment>
     );
   }
---
+++ src/features/events/EventDetailed/EventdetailedPage.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+const EventdetailedPage = () => {
+  return (
+    <div>
+      <h1>Event detailed page</h1>
+    </div>
+  );
+};
+export default EventdetailedPage;
